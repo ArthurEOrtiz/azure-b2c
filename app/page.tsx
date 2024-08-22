bf24d9acb65103671e6cc59e2e8a4930d32a1632
Arthur E Ortiz <100542148+ArthurEOrtiz@users.noreply.github.com>
2024-08-22 11:24:10 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-
 const Home: React.FC = () => {
   return (
-    <div className="flex-grow flex flex-col items-center justify-center">
+    <div className="flex-grow flex flex-col items-center justify-center space-y-2">
       <h1 className="text-3xl font-bold">Hello World!</h1>
-      <p className="text-lg">
-        This is an application to test authentication with Azure B2C. This is the home page, no authentication is required to view this page.
-      </p>
+      <div className="w-2/3 border rounded-xl p-2">
+        <p className="text-lg">
+          This is an application to test authentication with Azure B2C. This is the home page, no authentication is required to view this page.
+        </p>
+      </div>
     </div>
   );
 }
