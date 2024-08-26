7c75a04eeae45fcfc7da5dfbdf33aeb988d4a48e
Arthur E Ortiz <100542148+ArthurEOrtiz@users.noreply.github.com>
2024-08-26 11:05:34 -0600

    Update restricted-client-page.tsx
    Displays more raw data.

--- components/restricted-client-page.tsx
+++ components/restricted-client-page.tsx
@@ -1,25 +1,26 @@
 "use client";
 import { signOut, useSession } from "next-auth/react";
+import SignOutButton from "./azure/signout-button";
 
 
 const RestrictedClientPage: React.FC = () => {
-    const { data: session } =  useSession();
-    
+    const { data: session, } =  useSession();
 
     if (session) {
         return (
             <>
-                <div className="border rounded-xl p-2 space-y-2">
-                    <div>
-                        <p className="text-xl font-bold">You are authenticated!</p>
-                        <p>Email: {session.user?.email}</p>
+                <div className="space-y-2">
+                    <div className="flex flex-col items-center space-y-2">
+                        <h1 className="text-3xl font-bold">You are authenticated</h1>
+                        <div className="border rounded-xl p-2">
+                            <p className="text-lg">Session:</p>
+                            <pre>{JSON.stringify(session, null, 2)}</pre>
+                        </div>
+                        
                     </div>
-                    <button 
-                        className="btn btn-error text-white p-1"
-                        onClick={() => signOut({ callbackUrl: "/" })}
-                    >
-                        Sign Out
-                    </button>
+                    <div className="w-full flex justify-end">
+                        <SignOutButton />
+                    </div>
                 </div>
             </>
         );
